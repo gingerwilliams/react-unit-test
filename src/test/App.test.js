e90ca8f1ca1b5714ca3efc7f7c47ae6b064d9c25
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-08-21 11:02:47 -0400

    installed testing framework mocha and extension chai

---
+++ src/test/App.test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect; 
+const hello = require('../components/App');
+
+describe('hello-world', () => {
+    it('should work',() =>{
+        expect(true).to.be.true;
+    })
+});
