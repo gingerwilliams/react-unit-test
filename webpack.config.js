437e00eee8ae1dcbf9e5d9cc41d476833e304e55
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-08-20 23:00:32 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        publicPath: '/dist/',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        open: true,
+    }
+
+}
