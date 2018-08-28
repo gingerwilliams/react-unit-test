277f4efbbb10f5f1cda1f988936d2f1d484bcd2d
Ginger Williams <gingerwilliams@Gingers-MacBook-Pro.local>
2018-08-27 23:29:18 -0400

    wrapper and new config applied

--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 ## Enzyme 
-Enzyme expects an adapter to be configured. To configure an adapter, youshould call `Enzyme.configure({ adapter: new Adapter() })` before using any of Enzyme's top level APIs, where `Adapter` is the adapter corresponding to the library currently being tested.
-
-For example:     
-import Adapter from 'enzyme-adapter-react-15'; 
 
 http://airbnb.io/enzyme/docs/installation/index.html
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,19 @@
-const expect = require('chai').expect; 
-const hello = require('../components/App');
+const expect = require('chai').expect;
+
+import App from '../components/App';
+const wrapper = shallow(<App />);
 
 describe('hello-world', () => {
-    it('should work',() =>{
+    it('should work', () => {
         expect(true).to.be.true;
+    })
+});
+
+describe('Testing App Component<App/>', () => {
+    it('renders h1', () => {
+        expect(wrapper.find('h1').text()).equal('Welcome to React')
+    })
+    it('renders p', () => {
+        expect(wrapper.find('p').text()).equal('Get started')
     })
 });
